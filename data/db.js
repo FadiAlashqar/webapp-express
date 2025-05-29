193a5e390dcc41acb1e98c6a9e28ba082d2e45dd
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-30 00:15:36 +0200

    [UPD] mysql imported

---
+++ data/db.js
@@ -0,0 +1,20 @@
+// importo mwsql
+const mysql = require("mysql2");
+
+// cre i parametri per la connesione
+const connection = mysql.createConnection({
+    host: "",
+    user: "",
+    name: "",
+    password: ""
+})
+
+// creo la connessione
+connection.connect((err) => {
+    if (err) {
+        console.log("Connection to mySQL failed ")
+    }
+    console.log("Connected to mySQL!")
+});
+
+module.exports = connection;
