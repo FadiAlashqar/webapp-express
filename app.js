a40e438e21cf5e0eabc1ca4f00bfa578ab2c1f16
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-30 00:04:53 +0200

    [UPD] default route done

---
+++ app.js
@@ -0,0 +1,24 @@
+// importo express
+const express = require("express");
+
+// inizializzo express in una variabile
+const app = express();
+
+// dichiaro la porta nella variabile
+const port = 3000;
+
+// middleware asset statici
+app.use(express.static("public"));
+
+// middlware parsing
+app.use(express.json());
+
+// dichiaro la rotta base 
+app.get("/", (req, res) => {
+    res.send("Film's Server");
+})
+
+// tengo in ascolto il server 
+app.listen(port, () => {
+    console.log(`Servere listening on port : ${port}`)
+})
